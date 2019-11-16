9b2fa67e96c2b3b6c7638532a0fbb97de5780f90
Talha Ekhlas <ekhlas.cse08@gmail.com>
2019-11-16 20:59:34 +0600

    drag and drop try

--- First/custom.js
+++ First/custom.js
@@ -13,7 +13,8 @@ function dragEnter(e){
 
     event.target.style.border = "3px dotted black";
 
-  console.log('drag enter');}
+  
+}
 
 }
 
@@ -23,8 +24,8 @@ function dragLeave(e){
   if(e.target.className=='drop-target'){
 
     event.target.style.border = "";
-
-  console.log('drag leave');}
+    console.log('drag leave');
+}
 
 }
 
@@ -33,17 +34,15 @@ function dragLeave(e){
 function drag(ev) {
   var dragedDiv = document.querySelector('#'+ev.target.id).parentNode;
 
-  dragedDiv.style.background = "#D0D0D0";
-
   ev.dataTransfer.setData("html", ev.target.id);
 }
 
-function drop(ev,obj) {
+function drop(ev) {
   ev.preventDefault();
+
    
   var data = ev.dataTransfer.getData("html");
 
-  console.log(document.getElementById(data))
 
   ev.target.appendChild(document.getElementById(data));
 
@@ -51,7 +50,6 @@ function drop(ev,obj) {
 
   var droppedDiv = document.querySelector('#'+ev.target.id);
 
-  console.log(ev.target.id);
   droppedDiv.style.background = "red";
 }
 
@@ -63,22 +61,25 @@ function drop(ev,obj) {
 
 const treeArray = [{'name':'Beverages',
                    'child':[
-                             {'name':'Water','child':null},
-                             {'name':'tea','child':[
-                                                      {'name':'Black tea','child':null},
-                                                      {'name':'white tea','child':null},
-                                                      {'name':'green tea','child':[
-                                                                                    {'name':'khub valo','child':null},
-                                                                                    {'name':'motamuli valo','child':null},
-                                                                                    {'name':'Kharap','child':[
-                                                                                            {'name':'khub valo1','child':null},
-                                                                                         {'name':'motamuli valo1','child':null},
-                                                                                        {'name':'Kharap1','child':null}
-                                                                                        ]
-                                                                                    }
-                                                                                  ]
-                                                      }
+                             {'name':'Water',
+                             'child':null
+                             },
+                             {'name':'tea',
+                               'child':[
+                                          {'name':'Black tea','child':null},
+                                          {'name':'white tea','child':null},
+                                          {'name':'green tea','child':[
+                                              {'name':'khub valo','child':null},
+                                              {'name':'motamuli valo','child':null},
+                                              {'name':'Kharap','child':[
+                                                    {'name':'khub valo1','child':null},
+                                                    {'name':'motamuli valo1','child':null},
+                                                    {'name':'Kharap','child':null}
                                                   ]
+                                                }
+                                                                                    ]
+                                          }
+                                    ]
                               },
                              {'name':'Coffee','child':null},
                              
@@ -91,19 +92,27 @@ const treeArray = [{'name':'Beverages',
 
 //var treeList = '';
 //var treeNumber = 0;
+
+var serial =0;
 function treeGenerate(arrayForTraverse){
 
    var treeList = '';
       arrayForTraverse.map((item,index)=>{
+
+        serial++;
+
+        var dragId = 'drag'+serial;
         
 
         if(item.child!=null){
-             treeList +='<li><span class="caret" draggable="true" ondragstart="drag(event)" id="drag1">'+ item.name +'</span><ul class="nested">'+ treeGenerate(item.child) +'</ul></li>';
+
+             treeList +='<li draggable="true" ondragstart="drag(event)" id="'+dragId+'">'+'<span class="caret parent-project">'+ item.name +'</span><ul class="nested">'+ treeGenerate(item.child) +'</ul></li>';
+
 
 
         }else{
 
-          treeList +='<li><span  draggable="true" ondragstart="drag(event)" id="drag1">'+item.name+'</span></li>';
+          treeList +='<li class="child-project"  draggable="true" ondragstart="drag(event)" id="'+dragId+'">'+'<span >'+ item.name +'</span></li>';
         }
 
 
