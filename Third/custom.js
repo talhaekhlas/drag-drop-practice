18f3c31fc95af9cbd8340aacdf6e7c1e8a404416
Talha Ekhlas <ekhlas.cse08@gmail.com>
2019-11-17 16:15:27 +0600

    sfs

--- Third/custom.js
+++ Third/custom.js
@@ -1,14 +1,54 @@
-function onDragOver(e) {
-  e.preventDefault();
-}
 
 function onDragStart(e) {
   e.dataTransfer.setData("html", e.target.id);
 }
 
+function onDragEnter(e){
+	 
+}
+
+function onDragOver(e) {
+  e.preventDefault();
+
+}
+
 function onDrop(e) {
   e.preventDefault();
+
+  
   var dragId = e.dataTransfer.getData("html")
   var dragData = document.getElementById(dragId)
-  e.target.appendChild(dragData);
+
+  
+  // e.target.appendChild(dragData)
+   console.log(dragData)
+
+   if(!e.target.contains(dragData)){
+
+   	 console.log('node name',dragData.nodeName)
+
+  	 e.target.appendChild(dragData)
+  }else{
+  	 console.log('has this child node')
+  }
+
+
+
+  // console.log(e.target.id,dragData);
+
+  // prevParent = dragData.parentNode.id
+
+  // if(prevParent == 'to'){
+  	
+  // 	var DroppedDiv = document.getElementById('from');
+  // 	DroppedDiv.appendChild(dragData)
+  	
+  // }else{
+  	
+  // 	var DroppedDiv = document.getElementById('to');
+  // 	DroppedDiv.appendChild(dragData)
+    
+
+  // }
+  
 }
