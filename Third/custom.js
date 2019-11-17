47c864ad5532e6b49486c6c2a49a9beb90710f86
Talha Ekhlas <ekhlas.cse08@gmail.com>
2019-11-17 22:29:30 +0600

    alhamdulillah

--- Third/custom.js
+++ Third/custom.js
@@ -1,3 +1,6 @@
+var LiParentNode = document.querySelector('li');
+
+console.log(LiParentNode.parentNode.id);
 
 function onDragStart(e) {
   e.dataTransfer.setData("html", e.target.id);
@@ -19,18 +22,92 @@ function onDrop(e) {
   var dragId = e.dataTransfer.getData("html")
   var dragData = document.getElementById(dragId)
 
+  var dragParentId = dragData.parentNode.id;
+  var targetId = e.target.id;
+
+  
+  console.log('target id:',targetId)
+  console.log('target',e.target)
+
+  if((targetId == 'from' || targetId == 'to') ){
+      console.log('from or to')
+      if(targetId!=dragParentId){
+        e.target.appendChild(dragData)
+      }
+
+  }
+
+
+  if(e.target.nodeName == 'LI'){
+      console.log('li')
+      if(targetId!=e.target.parentNode.id){
+        e.target.parentNode.appendChild(dragData)
+      }
+
+      
+      
+  }
+
+  if(e.target.nodeName == 'SPAN'){
+
+    console.log('span')
+
+      if(targetId!=e.target.parentNode.parentNode.id){
+        e.target.parentNode.parentNode.appendChild(dragData)
+      }
+
+      
+  }
+
+
+
+  // if((targetId == 'from' || targetId == 'to') && targetId!=dragParentId){
+
+  //     e.target.appendChild(dragData)
+
+
+  // }else{
+  //   var targetParentParent = e.target.parentNode.parentNode;
+
+  //   if((targetParentParent != dragParentId) && (targetId!=dragParentId)){
+  //     e.target.parentNode.parentNode.appendChild(dragData)
+  //   }
+
+  //   if(e.target.nodeName=="LI"){
+
+  //       console.log('node name',e.target.nodeName)
+  //       console.log('parentNodeId ',e.target.parentNode.id)
+
+  //       document.querySelector(e.target.nodeName).parentNode.appendChild(dragData)
+
+
+
+        
+
+  //   }
+
+    
+    
+
+    // console.log('parent parent node',targetParentParent)
+
+
+  // }
+
+
+
   
   // e.target.appendChild(dragData)
-   console.log(dragData)
+   // console.log('target',e.target)
 
-   if(!e.target.contains(dragData)){
+  //  if(!e.target.contains(dragData)){
 
-   	 console.log('node name',dragData.nodeName)
+  //  	 console.log('node name',dragData.nodeName)
 
-  	 e.target.appendChild(dragData)
-  }else{
-  	 console.log('has this child node')
-  }
+  // 	 e.target.appendChild(dragData)
+  // }else{
+  // 	 console.log('has this child node')
+  // }
 
 
 
