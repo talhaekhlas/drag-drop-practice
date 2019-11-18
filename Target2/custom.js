3bc7ea3bcd388caba745bc2750bed5659078e6f9
Talha Ekhlas <ekhlas.cse08@gmail.com>
2019-11-18 22:25:42 +0600

    trying

---
+++ Target2/custom.js
@@ -0,0 +1,90 @@
+var LiParentNode = document.querySelector('li');
+
+console.log(LiParentNode.parentNode.id);
+
+function onDragStart(e) {
+  e.dataTransfer.setData("html", e.target.id);
+}
+
+function onDragOver(e) {
+  e.preventDefault();
+
+  console.log('on drag over')
+}
+
+function onDragLeave(e){
+    console.log('on drag leave')
+}
+
+function onDragEnter(e){
+   
+}
+
+
+
+function onDragEnd(e){
+  e.preventDefault();
+
+  console.log('on drag end')
+}
+
+function onDrop(e) {
+  e.preventDefault();
+
+  
+  var dragId = e.dataTransfer.getData("html")
+  var dragData = document.getElementById(dragId)
+
+  var dragParentId = dragData.parentNode.id;
+  var targetId = e.target.id;
+
+  
+  console.log('target id:',targetId)
+  console.log('target',e.target)
+
+  if((targetId == 'from' || targetId == 'to') ){
+      console.log('from or to')
+      if(targetId!=dragParentId){
+        e.target.appendChild(dragData)
+      }
+
+  }
+
+
+
+
+
+
+  if(e.target.nodeName == 'LI'){
+      console.log('li')
+      if(dragParentId!=e.target.parentNode.id){
+        e.target.parentNode.appendChild(dragData)
+      }else{
+        console.log('same li parent');
+      }
+
+      
+      
+  }
+
+  if(e.target.nodeName == 'SPAN'){
+
+      console.log('span')
+
+      if(dragParentId!=e.target.parentNode.parentNode.id){
+        e.target.parentNode.parentNode.appendChild(dragData)
+      }else{
+
+        var dropLiId = e.target.parentNode.nextSibling.nextSibling.insertAdjacentElement('afterend', dragData);
+
+
+        console.log('same span parent:',dropLiId);
+       
+      }
+
+      
+  }
+
+
+  
+}
